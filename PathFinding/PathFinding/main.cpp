0ce98a0045acdf70f9f823de72292010f80f78f1
You Wu <Uriana@dhcp-10-201-151-93.tamulink.tamu.edu>
2015-01-26 10:12:17 -0600

    initial program set up
    add project file and graph file

---
+++ PathFinding/PathFinding/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PathFinding
+//
+//  Created by Uriana on 1/25/15.
+//  Copyright (c) 2015 You Wu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Artificial Intelligence
 
 AI assignments here.
+
+Implemented under Xcode.
+
