c870f8802be0b3598b24ab1f4de63de068a2d976
urianawu <urianawu@tamu.edu>
2015-01-31 23:42:48 -0600

    finish main part of project, need to add some compiling logs to analyze performance..

--- PathFinding/main.cpp
+++ PathFinding/main.cpp
@@ -13,6 +13,19 @@
 
 using namespace std;
 
+//GET frontier next element
+//slightly different for queue and stack
+template <class T>
+Node* nextElement(T frontier)
+{
+    return frontier.top(); // for stack and priority_queue
+}
+
+template<> Node* nextElement(queue<Node*> frontier)
+{
+    return frontier.front(); //for stack
+}
+
 
 template <class T>
 Node* search(T frontier, Node* init, int goal)
@@ -21,7 +34,8 @@ Node* search(T frontier, Node* init, int goal)
     visited.push_back(init);
     frontier.push(init);
     while (!frontier.empty()) {
-        Node* testNode = frontier.front();
+        Node* testNode = nextElement(frontier);
+        //Node* testNode = frontier.top();
         frontier.pop();
         if (testNode->v == goal) {
             return testNode;
@@ -40,44 +54,92 @@ Node* search(T frontier, Node* init, int goal)
     return NULL;
 }
 
-//Node* Search(char* algorithm, Node* init, int* goal)
-//{
-//    
-//    switch (algorithm[0]) {
-//        case 'B':
-//            queue<Node*> frontier; //tbd
-//        break;
-//            case 'D':
-//            break;
-//            case 'G':
-//            break;
-//            
-//        default:
-//            cout << "Algorithm not correctly defined, please choose from: BFS / DFS / GBFS." << endl;
-//            break;
-//    }
-//    
-//}
 
-int main(int argc, char * argv[]) {
+int getVertexIndex(int x, int y, Graph* g)
+{
+    int index = -1;
+    for (int i = 0; i < g->nOfVertices*2; i+=2) {
+        if (x == g->vertices[i] && y == g->vertices[i+1]) {
+            index = i/2;
+        }
+    }
+    return index;
+}
+
+int main(int argc, char * argv[])
+{
     
     char filename[256];
-//    cout << "Please enter a graph file: ";
-//    cin.getline (filename, 256);
-//    cout << "Loading " << filename << endl;
-
+    //    cout << "Please enter a graph file: ";
+    //    cin.getline (filename, 256);
+    //    cout << "Loading " << filename << endl;
+    
     Graph* graph = new Graph;
     graph->read("ATM.graph");
     
-    Node* init = new Node(0, NULL);
-    init->setGraph(graph);
+    char searchAlgorithm[256];
+    int startX, startY, endX, endY;
+    cout << "BFS | DFS | GBFS :";
+    cin.getline(searchAlgorithm, 256);
     
-    queue<Node*> BFSfrontier;
-    Node* goal = search(BFSfrontier, init, 21);
-    cout << "solution: " << endl;
+    cout << "start point: ";
+    scanf ("%d %d",&startX, &startY);
+    
+    cout << "end point: ";
+    scanf("%d %d", &endX, &endY);
+    
+    int startI = getVertexIndex(startX, startY, graph);
+    if (startI == -1) {
+        cout << "start point not found!"<<endl;
+        return 0;
+    }
+    
+    int endI = getVertexIndex(endX, endY, graph);
+    if (endI == -1) {
+        cout << "goal point not found in graph!" << endl;
+        return 0;
+    }
+    
+    Node* init = new Node(startI, NULL);
+    init->setGraph(graph);
+    Node* goal;
+    
+    switch (searchAlgorithm[0]) {
+        case 'B':
+        case 'b':
+        {
+            queue<Node*> BFSfrontier;
+            goal = search(BFSfrontier, init, endI);
+            break;
+        }
+        case 'D':
+        case 'd':
+        {
+            stack<Node*> DFSfrontier;
+            goal = search(DFSfrontier, init, endI);
+            break;
+        }
+        case 'G':
+        case 'g':
+        {
+            priority_queue<Node*> GBFSfrontier;
+            goal = search(GBFSfrontier, init, endI);
+            break;
+        }
+        default:
+            cout << "Algorithm not correctly defined, please choose from: BFS / DFS / GBFS." << endl;
+            return 0;
+            break;
+    }
+    
+    
+    cout << "Solution path: " << endl;
     vector<Node*> path = goal->traceback();
-    for (int i = 0; i < path.size(); i++) {
+    for (int i = path.size()-1; i >= 0; i--) {
         cout << path.at(i)->v << " | ";
     }
+    cout << "search algorithm = " << searchAlgorithm;
+    cout << "total iterations = ";
+    
     return 0;
 }
